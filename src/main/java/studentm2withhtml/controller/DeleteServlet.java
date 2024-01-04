58d7bee200063c53acba2ef23795b5e73e473dc2
unknown <poojithadarshan2131@gmail.com>
2024-01-04 08:52:29 +0530

    fianl

--- src/main/java/studentm2withhtml/controller/DeleteServlet.java
+++ src/main/java/studentm2withhtml/controller/DeleteServlet.java
@@ -15,7 +15,7 @@ public class DeleteServlet extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	int id=Integer.parseInt(req.getParameter("id"));
-	
+//	HIi
 	StudentDao studentDao=new StudentDao();
 	Student student=studentDao.deleteStudentById(id);
 	if(student!=null) {
